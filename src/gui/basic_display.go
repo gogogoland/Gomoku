0484abff790b8ae1eb2dc1efc467aa7962b6cd5f
Charles Roy <croy@student.42.fr>
2017-01-16 00:19:46 +0100

    Add input function, Add little TODO

--- src/gui/basic_display.go
+++ src/gui/basic_display.go
@@ -1,7 +1,11 @@
 package gui
 
 import (
+	"bufio"
 	"fmt"
+	"strconv"
+	"os"
+	"strings"
 	"algo"
 )
 
@@ -14,4 +18,28 @@ func PrintBoard(GameBoard [][]int) {
 	for i := 0; i < len(GameBoard); i++{
 		fmt.Println(GameBoard[i])
 	}
+	X, Y := Input()
+	fmt.Println(X, Y)
+}
+
+/*
+ * Get X, Y and parse it.
+ * TODO 
+ * 	-> Do some check for the input.
+ *	-> Replace Value in board by 2 for player turn
+ *	-> Use AI
+*/
+
+func Input() (int, int) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter X Y value: ")
+	text, _ := reader.ReadString('\n')
+
+	text = strings.Replace(text,"\n", "", -1)
+	getValue := strings.Split(text, " ")
+	fmt.Println(getValue)
+
+	X, _:= strconv.Atoi(getValue[0])
+	Y, _:= strconv.Atoi(getValue[1])
+	return X, Y
 }
