55342e36106e9f34d9b5e49925191ec9f385d6ba
Charles Roy <croy@student.42.fr>
2017-02-01 16:20:16 +0100

    Add board, and sprite, need to split my function and add pawn

--- src/gui/gui.go
+++ src/gui/gui.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	_ "image/jpeg"
 	_ "image/png"
+	"os"
+
+	"image"
+	"image/draw"
 
 	"github.com/google/gxui"
 	"github.com/google/gxui/drivers/gl"
@@ -11,14 +15,35 @@ import (
 )
 
 
+var (
+	Title string = "Gomoku"
+)
 /*
 ** Little test for key usage with gxui
 */
 
 func appMain(driver gxui.Driver){
-	theme := flags.CreateTheme(driver)
+	//Path to Image 
+	file := "sprites/GoBoard.png"
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Printf("Failed to open image '%s': %v\n", file, err)
+		os.Exit(1)
+	}
 
-	window := theme.CreateWindow(800, 600, "Open file...")
+	source, _, err := image.Decode(f)
+	if err != nil {
+		fmt.Printf("Failed to read image '%s': %v\n", file, err)
+		os.Exit(1)
+	}
+
+	theme := flags.CreateTheme(driver)
+	img := theme.CreateImage()
+
+	window := theme.CreateWindow(800, 800, Title)
+	window.SetScale(flags.DefaultScaleFactor)
+	window.AddChild(img)
+
 	window.OnKeyDown(func(ev gxui.KeyboardEvent) {
 		if ev.Key == gxui.KeyEscape || ev.Key == gxui.KeyKpEnter {
 			fmt.Println("Close")
@@ -29,8 +54,15 @@ func appMain(driver gxui.Driver){
 	window.OnClick(func(me gxui.MouseEvent) {
 		if me.Button == 0 {
 			fmt.Println("Right Click.")
+			fmt.Println(me.WindowPoint)
 		}
 	})
+
+	// Copy the image to a RGBA format before handing to a gxui.Texture
+	rgba := image.NewRGBA(source.Bounds())
+	draw.Draw(rgba, source.Bounds(), source, image.ZP, draw.Src)
+	texture := driver.CreateTexture(rgba, 1)
+	img.SetTexture(texture)
 
 	window.OnClose(driver.Terminate)
 }
